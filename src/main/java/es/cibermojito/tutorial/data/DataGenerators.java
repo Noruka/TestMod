d0e36804be9cfe940b45148193f520b72728dad2
Guillem Viada <guillem.viada@auraree.com>
2021-02-25 21:43:50 +0100

    registrators

---
+++ src/main/java/es/cibermojito/tutorial/data/DataGenerators.java
@@ -0,0 +1,16 @@
+package es.cibermojito.tutorial.data;
+
+import es.cibermojito.tutorial.TutorialMod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    private DataGenerators() {}
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event){
+
+    }
+}
---
+++ src/main/java/es/cibermojito/tutorial/data/client/ModItemModelProvider.java
@@ -0,0 +1,18 @@
+package es.cibermojito.tutorial.data.client;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+
+
+    public ModItemModelProvider(DataGenerator generator, String modid, ExistingFileHelper existingFileHelper) {
+        super(generator, modid, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+
+    }
+}
